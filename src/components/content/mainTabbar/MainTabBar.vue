f05fc2e19253a4bcd0c3c859b203903c3929fc0d
coda <1262244864@qq.com>
2022-03-30 17:58:50 +0800

    home nav

--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -1,6 +1,6 @@
 <template>
     <tab-bar>
-      <tab-bar-item path="/home" activeColor="">
+      <tab-bar-item path="/home">
         <img slot="item-icon" src="~assets/img/tabbar/home.svg">
         <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg">
         <div slot="item-text">首页</div>
